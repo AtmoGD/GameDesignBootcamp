77b38de14fe58d7294f3177e3f4ec38e002520d1
Dennis Hawran <dennishawran@gmail.com>
2023-08-04 14:53:29 +0100

    Changed Folder Structure & imported ui sprites & ui sounds

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+    public int maxJumps = 2;
+    public float gravity = 20f;
+    public float rotationSpeed = 5f;
+}
